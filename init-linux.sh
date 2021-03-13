ba252cc2d15408faec834e2015aa10a34b1d7dfe
AntoineSavage <antoinesavage@hotmail.com>
2021-03-13 17:38:51 -0500

    Update init-linux.sh

--- init-linux.sh
+++ init-linux.sh
@@ -3,6 +3,16 @@
 # copy/paste the following line
 # mv /mnt/c/Users/antoi/Desktop/init-linux.sh ~/init-linux.sh && sh ~/init-linux.sh
 
+# haskell projects:
+# create new file hie.yaml with content:
+# cradle:
+#   stack:
+
+# If localhost is unreachable from windows
+# (from linux) ip addr | grep eth0
+# copy the ip after 'inet'
+# it's the same ip displayed by elm-app start
+
 echo "===================="
 echo "Create temp directory"
 mkdir init-linux-tmp
@@ -39,6 +49,17 @@ sudo create-elm-app temp1
 create-elm-app temp2
 cd temp2
 sudo elm-app test
+cd ..
+
+echo "===================="
+echo "Install haskell stack"
+wget -qO- https://get.haskellstack.org/ | sh
+echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
+stack update
+stack upgrade
+stack new temp3
+cd temp3
+stack test
 cd ..
 
 echo "===================="
