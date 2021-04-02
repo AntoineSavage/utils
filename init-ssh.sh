18c0c10bc5c51487820fd03eb84e7e528516081b
AntoineSavage <antoinesavage@hotmail.com>
2021-04-02 13:29:40 -0400

    Update init-ssh.sh

--- init-ssh.sh
+++ init-ssh.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 # copy/paste the following line:
-# sudo wget https://raw.githubusercontent.com/AntoineSavage/utils/main/init-ssh.sh && sh init-ssh.sh
+# sudo wget https://raw.githubusercontent.com/AntoineSavage/utils/main/init-ssh.sh && sudo sh init-ssh.sh
 
-ssh-keygen -t rsa -b 4096 -C "antoine.savage@github.com"
+sudo ssh-keygen -t rsa -b 4096 -C "antoine.savage@github.com"
