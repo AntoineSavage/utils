434361fe4eef4ead75a62d1b1b11447c16a69a3a
AntoineSavage <antoinesavage@hotmail.com>
2021-04-02 13:36:46 -0400

    Update init-ssh.sh

--- init-ssh.sh
+++ init-ssh.sh
@@ -3,4 +3,16 @@
 # copy/paste the following line:
 # sudo wget https://raw.githubusercontent.com/AntoineSavage/utils/main/init-ssh.sh && sudo sh init-ssh.sh
 
+# Then go to: https://github.com/settings/ssh/new
+# Run: cat ~/.ssh/id_rsa.pub
+# Copy/paste the output in the browser window
+
+echo "===================="
+echo "Create SSH key"
 sudo ssh-keygen -t rsa -b 4096 -C "antoine.savage@github.com"
+eval `ssh-agent -s`
+ssh-add ~/.ssh/id_rsa
+
+echo "===================="
+echo "Clean-up"
+rm init-ssh.sh
