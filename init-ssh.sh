95d9ad15ac5a3da3c4bef336b817a4626e43e165
Antoine Savage <antoine.savage@gmail.com>
2021-04-03 15:08:35 -0400

    Update init-ssh.sh

--- init-ssh.sh
+++ init-ssh.sh
@@ -3,18 +3,20 @@
 # copy/paste the following line:
 # wget -q https://raw.githubusercontent.com/AntoineSavage/utils/main/init-ssh.sh && sh init-ssh.sh
 
-# Then go to: https://github.com/settings/ssh/new
-# Run: cat ~/.ssh/id_rsa.pub
-# Copy/paste the output in the browser window
-
 # Make sure to enter a strong password
-ssh-keygen -t rsa -b 4096 -C "antoine.savage@github.com"
+ssh-keygen -t ed25519 -C "antoine.savage@github.com"
+chmod 600 ~/.ssh/id_ed25519
+chmod 600 ~/.ssh/id_ed25519.pub
 
 if ! pgrep -x "ssh-agent" > /dev/null
 then eval `ssh-agent -s`
 fi
 
-ssh-add ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_ed25519
 
 # Clean-up
 rm init-ssh.sh
+
+# Then go to: https://github.com/settings/ssh/new
+# cat ~/.ssh/id_ed25519.pub
+# Copy/paste the output in the browser window
