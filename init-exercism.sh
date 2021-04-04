8d31ba65c8bfd6a3cc5b9801f7d6aaa01f6416e4
AntoineSavage <antoinesavage@hotmail.com>
2021-04-04 15:25:08 -0400

    Create init-exercism.sh

---
+++ init-exercism.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# copy/paste the following line
+# wget -O init-linux.sh https://raw.githubusercontent.com/AntoineSavage/utils/main/init-exercism.sh && bash init-exercism.sh
+
+# Then get token from: https://exercism.io/my/settings
+# exercism configure --token=THE_TOKEN
+
+sudo echo ''
+
+rm -rf temp
+mkdir temp
+cd temp
+
+wget https://github.com/exercism/cli/releases/download/v3.0.13/exercism-linux-64bit.tgz
+tar -xf exercism-linux-64bit.tgz
+sudo mv exercism /usr/bin
+exercism version
+
+cd ..
+rm -rf temp
