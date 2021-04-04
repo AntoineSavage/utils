220059cd03a50ba424c02ea403ab5684e7c38ada
AntoineSavage <antoinesavage@hotmail.com>
2021-04-04 12:25:05 -0400

    Update init-haskell.sh

--- init-haskell.sh
+++ init-haskell.sh
@@ -5,6 +5,7 @@
 
 rm -rf temp
 stack new temp
+cd temp
 stack install aeson async doctest hspec parsec postgresql-typed QuickCheck sensei servant stm text wai wai-websockets warp websockets
 stack build
 
