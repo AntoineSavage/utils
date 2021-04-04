f1add6880694f6da44395289923a5fc0b94eddf0
AntoineSavage <antoinesavage@hotmail.com>
2021-04-04 12:23:51 -0400

    Update init-haskell.sh

--- init-haskell.sh
+++ init-haskell.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
+
+# copy/paste the following line
+# wget -q https://raw.githubusercontent.com/AntoineSavage/utils/main/init-haskell.sh && bash init-haskell.sh
+
+rm -rf temp
 stack new temp
 stack install aeson async doctest hspec parsec postgresql-typed QuickCheck sensei servant stm text wai wai-websockets warp websockets
 stack build
