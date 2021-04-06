cba07b37f8aa78909d4f555b80d0299c29e02d2d
AntoineSavage <antoinesavage@hotmail.com>
2021-04-06 17:59:23 -0400

    Update init-linux.sh

--- init-linux.sh
+++ init-linux.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-# copy/paste the following line
-# wget -qO init-linux.sh https://raw.githubusercontent.com/AntoineSavage/utils/main/init-linux.sh && bash init-linux.sh && source ~/.bashrc
-
-# Add SSH key to github account: https://github.com/settings/ssh/new
-
-# Elm format-on-save in vscode:
-# Ctrl+Shift+P, open settings (JSON)
-# Add the following key:
-# "[elm]": {
-#     "editor.formatOnSave": true
-# },
-
-# If localhost is unreachable from windows
-# (from linux) ip addr | grep eth0
-# copy the ip after 'inet'
-# it's the same ip displayed by elm-app start
-
-# Some operations are user-specific
 if [ `whoami` = 'root' ]; then echo "This program must NOT be run as 'sudo'"; exit; fi
 
 echo "===================="
