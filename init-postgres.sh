c78fc6b3157dcce6a513631433a36e99db69c9d1
AntoineSavage <antoinesavage@hotmail.com>
2021-04-06 13:10:51 -0400

    Update init-postgres.sh

--- init-postgres.sh
+++ init-postgres.sh
@@ -2,6 +2,7 @@
 # this script is called by init-linux.sh
 
 pg_ctlcluster 13 main start
+psql -c '\\password postgres'
 psql -c 'create database dvdrental;'
 
 curl -O https://sp.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip
