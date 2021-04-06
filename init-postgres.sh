4364547af41db1f143f21b562d2c7a81eebf8efc
AntoineSavage <antoinesavage@hotmail.com>
2021-04-06 13:42:48 -0400

    Update init-postgres.sh

--- init-postgres.sh
+++ init-postgres.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # this script is called by init-linux.sh
 
-# pg_ctlcluster 13 main start
+pg_ctlcluster 13 main start
 psql -U postgres -c "ALTER USER postgres WITH PASSWORD 'postgres';"
-# psql -c 'create database dvdrental;'
+psql -c 'create database dvdrental;'
 
-# curl -O https://sp.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip
-# unzip dvdrental.zip
-# rm dvdrental.zip
+curl -O https://sp.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip
+unzip dvdrental.zip
+rm dvdrental.zip
 
-# pg_restore --dbname=dvdrental --verbose dvdrental.tar
-# rm dvdrental.tar
+pg_restore --dbname=dvdrental --verbose dvdrental.tar
+rm dvdrental.tar
 
 rm init-postgres.sh
