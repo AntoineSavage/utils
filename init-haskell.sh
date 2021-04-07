c2707a759fdcd7c55a36f0baafe994f6780ac425
AntoineSavage <antoinesavage@hotmail.com>
2021-04-07 17:09:44 -0400

    Update init-haskell.sh

--- init-haskell.sh
+++ init-haskell.sh
@@ -3,7 +3,7 @@
 rm -rf temp
 stack new temp
 cd temp
-stack install aeson async doctest hspec parsec postgresql-typed QuickCheck sensei servant stm text time utf8-string wai wai-websockets warp websockets
+stack install aeson async containers doctest hspec parsec postgresql-typed QuickCheck sensei servant stm text time utf8-string wai wai-websockets warp websockets
 stack build
 
 cd ..
