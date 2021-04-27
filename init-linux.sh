81382700ca6849b7deed09d67e4795c4894e324a
AntoineSavage <antoinesavage@hotmail.com>
2021-04-27 19:20:00 -0400

    Update init-linux.sh

--- init-linux.sh
+++ init-linux.sh
@@ -48,47 +48,8 @@ sudo apt update
 sudo apt upgrade -y
 
 echo "===================="
-echo "Install packages (pip, npm, elixir, postgresql, openjdk, docker, ...)"
-sudo apt install -y python3-pip npm elixir postgresql postgresql-contrib openjdk-13-jdk docker.io maven keychain zip unzip libtinfo-dev g++ gcc libc6-dev libffi-dev libgmp-dev make xz-utils zlib1g-dev git gnupg netbase
-npm install -g npm
-
-echo "===================="
-echo "Install elm modules"
-npm install -g elm elm-format elm-test
-npm install -g create-elm-app # must be installed separately
-
-echo "===================="
-echo "Install haskell"
-wget https://get.haskellstack.org/stable/linux-x86_64.tar.gz
-gzip -d linux-x86_64.tar.gz
-tar -xvf linux-x86_64.tar
-sudo install -c -o 0 -g 0 -m 0755 stack*linux*/stack /usr/local/bin
-stack update
-stack upgrade
-sed -i 's/#    author-name:/    author-name: Antoine Savage/g' ~/.stack/config.yaml
-sed -i 's/#    author-email:/    author-email: antoine.savage@gmail.com/g' ~/.stack/config.yaml
-sed -i 's/#    github-username:/    github-username: AntoineSavage/g' ~/.stack/config.yaml
-sed -i 's/#    copyright://g' ~/.stack/config.yaml
-
-echo "===================="
-echo "Init postgres DB"
-rm -rf temp
-mkdir temp
-cd temp
-echo 'postgres:postgres' | sudo chpasswd
-sudo service postgresql start
-sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
-sudo -u postgres psql -c "create database dvdrental;"
-curl -O https://sp.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip
-unzip dvdrental.zip
-sudo -u postgres pg_restore --dbname=dvdrental --verbose dvdrental.tar
-sudo service postgresql stop
-cd ..
-rm -rf temp
-
-echo "===================="
-echo "Init docker"
-sudo usermod -aG docker asavage
+echo "Install dependency packages"
+sudo apt install -y keychain zip unzip libtinfo-dev g++ gcc libc6-dev libffi-dev libgmp-dev make xz-utils zlib1g-dev git gnupg netbase
 
 echo "===================="
 echo "Clean-up"
@@ -97,6 +58,3 @@ sudo apt autoremove
 rm -rf init-linux-tmp
 rm init-linux.sh
 kill "$refresh_sudo"
-
-echo "===================="
-echo "Completed. Please log out and log back in for docker group changes to take effect"
