c2d615fa7042cb68076d98dc89470a49b898773d
AntoineSavage <antoinesavage@hotmail.com>
2021-04-28 10:10:50 -0400

    Update init-git.sh

--- init-git.sh
+++ init-git.sh
@@ -2,17 +2,18 @@
 
 if [ `whoami` = 'root' ]; then echo "This program must NOT be run as 'sudo'"; exit; fi
 
+echo "Installing keychain..."
+sudo apt -qq update
+sudo apt -qq install keychain
+
+echo "Configuring ssh for git..."
 git config --global user.name "Antoine Savage"
 git config --global user.email "antoine.savage@gmail.com"
 
-sudo apt update
-sudo apt install keychain
-
-ssh-keygen -f ~/.ssh/id_ed25519 -N '' -t ed25519 -C "antoine.savage@github.com"
+ssh-keygen -q -t ed25519 -f ~/.ssh/id_ed25519 -N '' -C "antoine.savage@github.com" <<<y 2>&1 >/dev/null
 chmod 600 ~/.ssh/id_ed25519
 chmod 600 ~/.ssh/id_ed25519.pub
 echo 'eval $(keychain --eval id_ed25519)' >> ~/.bashrc
 
-echo "===================="
 echo "Add the following to github account: https://github.com/settings/ssh/new"
 cat ~/.ssh/id_ed25519.pub
