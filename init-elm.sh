575527470c8cf41787e8e20ca7e9990b694b49d2
AntoineSavage <antoinesavage@hotmail.com>
2021-04-28 18:55:16 -0400

    Update init-elm.sh

--- init-elm.sh
+++ init-elm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ `whoami` != 'root' ]; then echo "This program must be run using 'sudo'"; exit; fi
+if [ `whoami` = 'root' ]; then echo "This program must NOT be run using 'sudo'"; exit; fi
 
-chown -R asavage /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
+sudo chown -R $(whoami) /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
 
-apt update
-apt install -y npm
-sudo -u asavage npm install -g npm
-sudo -u asavage npm install -g elm elm-format elm-test
-sudo -u asavage npm install -g create-elm-app # must be installed separately
+sudo apt update
+sudo apt install -y npm
+npm install -g npm
+npm install -g elm elm-format elm-test
+npm install -g create-elm-app # must be installed separately
