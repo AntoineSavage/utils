5d97264b1b329636e644820a25128729b246c39a
AntoineSavage <antoinesavage@hotmail.com>
2021-04-28 18:55:45 -0400

    Update init-haskell.sh

--- init-haskell.sh
+++ init-haskell.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if [ `whoami` != 'root' ]; then echo "This program must be run using 'sudo'"; exit; fi
+if [ `whoami` = 'root' ]; then echo "This program must NOT be run using 'sudo'"; exit; fi
 
+sudo echo ''
 echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
 source ~/.bashrc
 
@@ -12,9 +13,9 @@ cd temp
 wget https://get.haskellstack.org/stable/linux-x86_64.tar.gz
 gzip -d linux-x86_64.tar.gz
 tar -xvf linux-x86_64.tar
-install -c -o 0 -g 0 -m 0755 stack*linux*/stack /usr/local/bin
-sudo -u asavage stack update
-sudo -u asavage stack upgrade
+sudo install -c -o 0 -g 0 -m 0755 stack*linux*/stack /usr/local/bin
+stack update
+stack upgrade
 sed -i 's/#    author-name:/    author-name: Antoine Savage/g' ~/.stack/config.yaml
 sed -i 's/#    author-email:/    author-email: antoine.savage@gmail.com/g' ~/.stack/config.yaml
 sed -i 's/#    github-username:/    github-username: AntoineSavage/g' ~/.stack/config.yaml
@@ -24,8 +25,8 @@ cd ..
 rm -rf temp
 stack new temp
 cd temp
-sudo -u asavage stack install aeson async containers doctest hspec parsec postgresql-typed QuickCheck sensei servant stm text time utf8-string wai wai-cors wai-websockets warp websockets
-sudo -u asavage stack build
+stack install aeson async containers doctest hspec parsec postgresql-typed QuickCheck sensei servant stm text time utf8-string wai wai-cors wai-websockets warp websockets
+stack build
 
 cd ..
 rm -rf temp
