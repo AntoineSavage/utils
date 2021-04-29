d823aec68c11e07d0c0dc864c3f08c738999aeaf
AntoineSavage <antoinesavage@hotmail.com>
2021-04-29 09:54:47 -0400

    Update init-postgres.sh

--- init-postgres.sh
+++ init-postgres.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ `whoami` != 'root' ]; then echo "This program must be run using 'sudo'"; exit; fi
+if [ `whoami` = 'root' ]; then echo "This program must NOT be run using 'sudo'"; exit; fi
 
-apt update
-apt install -y postgresql postgresql-contrib
+sudo apt update
+sudo apt install -y postgresql postgresql-contrib
 
 rm -rf temp
 mkdir temp
@@ -12,12 +12,12 @@ cd temp
 curl -O https://sp.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip
 unzip dvdrental.zip
 
-echo 'postgres:postgres' | chpasswd
-service postgresql start
+echo 'postgres:postgres' | sudo chpasswd
+sudo service postgresql start
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 sudo -u postgres psql -c "create database dvdrental;"
 sudo -u postgres pg_restore --dbname=dvdrental --verbose dvdrental.tar
-service postgresql stop
+sudo service postgresql stop
 
 cd ..
 rm -rf temp
