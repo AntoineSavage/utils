3181715fdf63bfbc847d588c5058d2c3d4aceb69
AntoineSavage <antoinesavage@hotmail.com>
2021-04-29 09:46:59 -0400

    Update init-packages.sh

--- init-packages.sh
+++ init-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ `whoami` != 'root' ]; then echo "This program must be run as 'sudo'"; exit; fi
+if [ `whoami` = 'root' ]; then echo "This program must NOT be run as 'sudo'"; exit; fi
 
-apt update
-apt upgrade -y
-apt install -y g++ gcc gnupg libc6-dev libffi-dev libgmp-dev libtinfo-dev make netbase unzip xz-utils zlib1g-dev zip
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y g++ gcc gnupg libc6-dev libffi-dev libgmp-dev libtinfo-dev make netbase unzip xz-utils zlib1g-dev zip
