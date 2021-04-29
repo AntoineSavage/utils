dda244701e19a0543bf6cc4977519ee784063451
AntoineSavage <antoinesavage@hotmail.com>
2021-04-29 09:36:46 -0400

    Update init-git.sh

--- init-git.sh
+++ init-git.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 if [ `whoami` = 'root' ]; then echo "This program must NOT be run using 'sudo'"; exit; fi
-
-sudo apt update
-sudo apt install -y keychain
+sudo -v
 
 git config --global user.name "Antoine Savage"
 git config --global user.email "antoine.savage@gmail.com"
@@ -15,3 +13,6 @@ echo 'eval $(keychain --eval id_ed25519)' >> ~/.bashrc
 
 echo "Add the following to github account: https://github.com/settings/ssh/new"
 cat ~/.ssh/id_ed25519.pub
+
+sudo apt update
+sudo apt install -y keychain
