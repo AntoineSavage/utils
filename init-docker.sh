582aa41308092831f83319ce5441f9df109333fd
AntoineSavage <antoinesavage@hotmail.com>
2021-04-29 09:50:45 -0400

    Update init-docker.sh

--- init-docker.sh
+++ init-docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ `whoami` != 'root' ]; then echo "This program must be run using 'sudo'"; exit; fi
+if [ `whoami` = 'root' ]; then echo "This program must NOT be run using 'sudo'"; exit; fi
 
-apt update
-apt install -y docker.io
-usermod -aG docker asavage
+sudo apt update
+sudo apt install -y docker.io
+sudo usermod -aG docker $(whoami)
 
 echo "Please log out and log back in to use docker"
 echo "Start docker deamon with: sudo dockerd"
