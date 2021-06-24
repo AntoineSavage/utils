921511769b8b0c0ef30546c6eb4c1925082213b9
AntoineSavage <antoinesavage@hotmail.com>
2021-06-24 19:21:40 -0400

    Delete init-gcloud.sh

--- init-gcloud.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ `whoami` = 'root' ]; then echo "This program must NOT be run as 'sudo'"; exit; fi
-
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt-get update && sudo apt-get install -y google-cloud-sdk google-cloud-sdk-app-engine-python google-cloud-sdk-app-engine-java google-cloud-sdk-datastore-emulator
