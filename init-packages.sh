1eec019501e8720a896c76168246d6444a0666e8
AntoineSavage <antoinesavage@hotmail.com>
2021-06-24 17:42:58 -0400

    Delete init-packages.sh

--- init-packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ `whoami` = 'root' ]; then echo "This program must NOT be run as 'sudo'"; exit; fi
-
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y g++ gcc gnupg libc6-dev libffi-dev libgmp-dev libtinfo-dev make netbase unzip xz-utils zlib1g-dev zip
