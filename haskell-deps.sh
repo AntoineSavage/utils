5d889c296eeca3642f252f2629a0685cbfafdf12
AntoineSavage <antoinesavage@hotmail.com>
2021-06-24 20:36:12 -0400

    Update haskell-deps.sh

--- haskell-deps.sh
+++ haskell-deps.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+if [ `whoami` = 'root' ]; then echo "This program must NOT be run using 'sudo'"; exit; fi
 
+rm -rf temp
+stack new temp
+cd temp
+stack install aeson async containers doctest hspec parsec postgresql-typed QuickCheck sensei servant stm text time utf8-string wai wai-cors wai-websockets warp websockets
+cd ..
+rm -rf temp
